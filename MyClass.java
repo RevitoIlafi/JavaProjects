3111874d4090693a218459ac9ce26af0db850906
Revito Ilafi <ilafirevito@gmail.com>
2023-05-13 14:37:11 +0700

    First Java Project

---
+++ MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass{
+    public static void main(String args[]){
+        final int x = 10;
+        Integer x2 = 12756810;
+        System.out.println(x + 17);
+        System.out.println(x2.toString().length());
+        System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
+    }
+}
